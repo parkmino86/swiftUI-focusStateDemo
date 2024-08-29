ac517ae895b47516b158533aa99439f91bb75114
minoh.park <parkmino86@gmail.com>
2024-08-29 21:25:48 +0900

    Initial Commit

---
+++ focusStateDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  focusStateDemo
+//
+//  Created by minoh.park on 8/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ focusStateDemo/focusStateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusStateDemoApp.swift
+//  focusStateDemo
+//
+//  Created by minoh.park on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct focusStateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
